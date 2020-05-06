aa69e65ae7c2ee1011f418c693726e273741c270
Varun <varun@ekasta.com>
2020-05-06 15:22:24 +0530

    add test to fail if content length does not match as mentioned in the header

--- test/request_test.cc
+++ test/request_test.cc
@@ -54,6 +54,24 @@ namespace {
       "Cookie: foo=bar; lorem=ipsum\r\n"
       "\r\n"
       "foo=bar&bar=foo";
+
+    std::string raw_invalid_request = "GET /foo/bar?name=octo HTTP/1.1\r\n"
+      "Host: example.org\r\n"
+      "User-Agent: Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; fr; rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8\r\n"
+      "Accept: */*\r\n"
+      "Accept-Language: fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3\r\n"
+      "Accept-Encoding: gzip,deflate\r\n"
+      "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7\r\n"
+      "Keep-Alive: 115\r\n"
+      "Connection: keep-alive\r\n"
+      "Content-Type: application/x-www-form-urlencoded\r\n"
+      "Content-Length: 15\r\n"
+      "X-Requested-With: XMLHttpRequest\r\n"
+      "Referer: http://example.org/test\r\n"
+      "Cookie: foo=bar; lorem=ipsum\r\n"
+      "\r\n"
+      "foo=bar&bar=foo\n"
+      "WOW THIS SHOULD FAIL";
 }
 
 TEST(HTTPRequest, RequestLineTest) {
@@ -79,4 +97,10 @@ TEST(HTTPRequest, HeadersTest) {
     ASSERT_EQ(host_header, "example.org");
     std::string powered_by_header = request.get_header("X-Powered-By");
     ASSERT_EQ(powered_by_header, "");
+}
+
+
+TEST(HTTPRequest, ContentLengthDoesNotMatch) {
+    octo::http::Request request;
+    ASSERT_FALSE(request.parse_http_request(raw_invalid_request));
 }
